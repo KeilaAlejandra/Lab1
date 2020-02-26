577327c8ca318279092e6416ba724ccedbb5a747
keila <castrokeila08@gmail.com>
2020-02-26 11:03:45 -0500

    gregue los controladores con sus vistas y la conexion

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MvcPlantilla.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AgregarVideo()
+        {
+            return View();
+        }
+        public ActionResult EliminarVideo()
+        {
+            return View();
+        }
+        public ActionResult ModificarVideo()
+        {
+            return View();
+        }
+        public ActionResult EliminarXReproduccion()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Video/Index.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Index</title>
+</head>
+<body>
+    <h1> AGREGAR VIDEO </h1>
+    <h1> ELIMINAR VIDEO </h1>
+    <h1>MODIFICAR VIDEO </h1>
+    <h1>ACTUALIZAR VIDEO </h1>
+</body>
+</html>
